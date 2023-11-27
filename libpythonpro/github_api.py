3f90d05beccd394b6c83ca1a0cf6602990501ad7
lucas <lucassilvarubio99@gmail.com>
2023-11-27 20:13:30 -0300

    Instalado o pip para utilizaçao de bibliotecas

--- libpythonpro/github_api.py
+++ libpythonpro/github_api.py
@@ -1,13 +1,17 @@
 import requests
 
-
 def buscar_avatar(usuario):
     """
-    Busca o avatar de um usuário no Github
+    Busca um avatar de um usuário no GitHub
 
-    :param usuario: str com o nome de usuário no github
+    :param usuairo: str com o nome de usuário no GitHub
     :return: str com o link do avatar
     """
     url = f'https://api.github.com/users/{usuario}'
     resp = requests.get(url)
     return resp.json()['avatar_url']
+
+if __name__ == '__main__':
+    print(buscar_avatar('lucasrubioo'))
+
+
